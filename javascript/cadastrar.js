336fc7cb177df46651ca1b2922df0a6a563aa2e8
Arthur Callegari <arthurcallegari1@gmail.com>
2024-01-05 14:52:56 -0300

    js

---
+++ javascript/cadastrar.js
@@ -0,0 +1,25 @@
+
+
+
+function limitarParaNumerosEspeciais() {
+    return document.getElementById("cep").textContent.replace(/[^0-9\W_]/, '');
+}
+
+function cadastrar() {
+
+    let email = document.getElementById("email");
+    let cpf = document.getElementById("cpf")
+    let nomeCompleto = document.getElementById("nomeCompleto");
+    let rua = document.getElementById("rua");
+    let nascimento = document.getElementById("nascimento");
+    let numero = document.getElementById("numero");
+    let complemento = document.getElementById("complemento");
+    let senha = document.getElementById("senha");
+    let confsenha = document.getElementById("confsenha");
+    let referencia = document.getElementById("referencia");
+    let cel1 = document.getElementById("cel1");
+    let cel2 = document.getElementById("cel2");
+ 
+    
+
+}
